cbee7cb99305368da4b87eb125751844ae34d258
Luciano Santos <contato@lucianosantos.dev>
2023-01-15 22:09:42 -0300

    Implement beep

--- wear/src/main/java/dev/lucianosantos/intervaltimer/core/data/TimerViewModel.kt
+++ wear/src/main/java/dev/lucianosantos/intervaltimer/core/data/TimerViewModel.kt
@@ -1,15 +1,19 @@
 package dev.lucianosantos.intervaltimer.core.data
 
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import android.text.format.DateUtils
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.ceil
+import kotlin.math.round
 import kotlin.time.Duration.Companion.seconds
-import kotlin.time.DurationUnit
-import kotlin.time.toDuration
+
 
 class TimerViewModel : ViewModel() {
 
@@ -22,20 +26,46 @@ class TimerViewModel : ViewModel() {
 
     fun startTimer() {
         Log.d(TAG, "Start timer")
-        val timer = object: CountDownTimer(20000, 1000) {
+        val timer = object: CountDownTimer(5000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
+                val seconds = round(millisUntilFinished / 1000.0).toLong()
+
                 _uiState.value?.let { currentUiState ->
                     _uiState.value = currentUiState.copy(
-                        currentTime = DateUtils.formatElapsedTime(millisUntilFinished / 1000)
+                        currentTime = DateUtils.formatElapsedTime(seconds)
                     )
                 }
-                Log.d(TAG, "${_uiState.value?.currentTime}")
+
+                verifyAndBeep(seconds)
             }
 
             override fun onFinish() {
+                _uiState.value?.let { currentUiState ->
+                    _uiState.value = currentUiState.copy(
+                        currentTime = DateUtils.formatElapsedTime(0),
+                        timerState = TimerState.FINISHED
+                    )
+                }
+                beep()
+                beep(200)
             }
         }
         timer.start()
+    }
+
+    private fun verifyAndBeep(seconds: Long) {
+        if (seconds > 3) {
+            return
+        }
+        beep()
+    }
+
+    private fun beep(delayMilliseconds : Long = 0) {
+        viewModelScope.launch {
+            delay(delayMilliseconds)
+            val toneGen1 = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+            toneGen1.startTone(ToneGenerator.TONE_CDMA_EMERGENCY_RINGBACK, 100)
+        }
     }
 
     data class UiState(
