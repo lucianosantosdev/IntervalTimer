b95b404b5102b37b5ba93b1ed86c8627bdcb419d
Luciano Santos <contato@lucianosantos.dev>
2023-01-26 17:57:52 -0300

    Change beep pattern on finish

--- core/src/main/java/dev/lucianosantos/intervaltimer/core/utils/BeepHelper.kt
+++ core/src/main/java/dev/lucianosantos/intervaltimer/core/utils/BeepHelper.kt
@@ -7,18 +7,12 @@ import kotlinx.coroutines.delay
 class BeepHelper : IBeepHelper {
     private val toneGenerator = ToneGenerator(AudioManager.STREAM_ALARM, ToneGenerator.MAX_VOLUME)
 
-    private suspend fun shortBeep() {
+    private fun shortBeep() {
         toneGenerator.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT, 100)
     }
 
-    private suspend fun longBeep() {
+    private fun longBeep() {
         toneGenerator.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT, 400)
-    }
-
-    private suspend fun doubleBeep() {
-        shortBeep()
-        delay(300)
-        shortBeep()
     }
 
     override suspend fun startPrepareBeep() {
@@ -30,10 +24,20 @@ class BeepHelper : IBeepHelper {
     }
 
     override suspend fun startRestBeep() {
-        doubleBeep()
+        shortBeep()
+        delay(300)
+        shortBeep()
     }
 
     override suspend fun finishedBeep() {
+        shortBeep()
+        delay(300)
+        shortBeep()
+        delay(300)
+        shortBeep()
+        delay(300)
+        longBeep()
+        delay(400)
         longBeep()
     }
 
