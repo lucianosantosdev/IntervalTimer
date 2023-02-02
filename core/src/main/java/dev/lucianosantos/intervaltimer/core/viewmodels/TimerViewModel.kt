01b7ca79f49acdfa505c71fba6ba6d1d4d852346
Luciano Santos <contato@lucianosantos.dev>
2023-02-02 18:12:37 -0300

    Change recursion to event channel

--- core/src/main/java/dev/lucianosantos/intervaltimer/core/viewmodels/TimerViewModel.kt
+++ core/src/main/java/dev/lucianosantos/intervaltimer/core/viewmodels/TimerViewModel.kt
@@ -1,18 +1,26 @@
 package dev.lucianosantos.intervaltimer.core.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.*
 import dev.lucianosantos.intervaltimer.core.data.TimerSettings
 import dev.lucianosantos.intervaltimer.core.data.TimerState
 import dev.lucianosantos.intervaltimer.core.utils.IAlertUserHelper
 import dev.lucianosantos.intervaltimer.core.utils.ICountDownTimerHelper
 import dev.lucianosantos.intervaltimer.core.utils.formatMinutesAndSeconds
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 class TimerViewModel(
     private val timerSettings: TimerSettings,
     private val countDownTimerHelper: ICountDownTimerHelper,
-    private val beepHelper: IAlertUserHelper
+    private val alertUserHelper: IAlertUserHelper
 ) : ViewModel() {
+
+    private val eventChannel = Channel<Event>()
+    private val eventsFlow = eventChannel.receiveAsFlow()
 
     private val _uiState: MutableLiveData<UiState> by lazy {
         MutableLiveData<UiState>(UiState(
@@ -22,11 +30,37 @@ class TimerViewModel(
         ))
     }
     val uiState get() : LiveData<UiState> = _uiState
+    
+    init {
+        setEventHandler()
+    }
+
+    private fun setEventHandler() {
+        eventsFlow.onEach {
+            when(it) {
+                is Event.Prepare -> {
+                    setCurrentState(TimerState.PREPARE)
+                    startCountDownTimer(timerSettings.prepareTimeSeconds)
+                }
+                is Event.Train -> {
+                    setCurrentState(TimerState.TRAIN)
+                    startCountDownTimer(timerSettings.trainTimeSeconds)
+                }
+                is Event.Rest -> {
+                    setCurrentState(TimerState.REST)
+                    startCountDownTimer(timerSettings.restTimeSeconds)
+                }
+                is Event.Finished -> {
+                    setRemainingSections(0)
+                    setCurrentState(TimerState.FINISHED)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
 
     fun startTimer() {
-        setCurrentState(TimerState.PREPARE)
-        startCountDownTimer(timerSettings.prepareTimeSeconds) {
-            trainAndRest(timerSettings.sections)
+        viewModelScope.launch {
+            eventChannel.send(Event.Prepare)
         }
     }
 
@@ -38,34 +72,56 @@ class TimerViewModel(
         countDownTimerHelper.resume()
     }
 
-    private fun trainAndRest(section: Int) {
-        setRemainingSections(section)
-        setCurrentState(TimerState.TRAIN)
-
-        startCountDownTimer(timerSettings.trainTimeSeconds) {
-            if ( section == 1 ) {
-                setRemainingSections(0)
-                setCurrentState(TimerState.FINISHED)
-                return@startCountDownTimer
-            }
-            setCurrentState(TimerState.REST)
-            startCountDownTimer(timerSettings.restTimeSeconds) {
-                trainAndRest(section - 1)
-            }
-        }
-    }
-
-    private fun startCountDownTimer(seconds: Int, onFinished: () -> Unit) {
+    private fun startCountDownTimer(seconds: Int) {
         setCurrentTime(seconds)
         countDownTimerHelper.startCountDown(seconds, { secondsUntilFinished ->
             setCurrentTime(secondsUntilFinished.toInt())
             if (secondsUntilFinished <= 3) {
-                notifyUserWithBeep(null)
+                alertUser(null)
             }
         }, onFinishCallback = {
             setCurrentTime(0)
-            onFinished()
+            resolveNextEvent()
         })
+    }
+
+    private fun resolveNextEvent() {
+        viewModelScope.launch {
+            val currentRemainingSections = _uiState.value?.remainingSections ?: 0
+            Log.d(TAG, "Current state: ${_uiState.value?.timerState} - Remaining sections : $currentRemainingSections")
+
+            when (_uiState.value?.timerState) {
+                TimerState.PREPARE -> {
+                    afterPrepare()
+                }
+                TimerState.REST -> {
+                    afterRest(currentRemainingSections)
+                }
+                TimerState.TRAIN -> {
+                    afterTrain(currentRemainingSections)
+                }
+                else -> {
+                    // Nothing to do here.
+                }
+            }
+        }
+    }
+
+    private suspend fun afterPrepare() {
+        eventChannel.send(Event.Train)
+    }
+
+    private suspend fun afterRest(remainingSections: Int) {
+        setRemainingSections(remainingSections - 1)
+        eventChannel.send(Event.Train)
+    }
+
+    private suspend fun  afterTrain(remainingSections: Int) {
+        if (remainingSections > 1) {
+            eventChannel.send(Event.Rest)
+        } else {
+            eventChannel.send(Event.Finished)
+        }
     }
 
     private fun setRemainingSections(sections: Int) {
@@ -90,17 +146,17 @@ class TimerViewModel(
                 timerState = timerState
             )
         }
-        notifyUserWithBeep(_uiState.value?.timerState)
+        alertUser(_uiState.value?.timerState)
     }
 
-    private fun notifyUserWithBeep(state: TimerState?) {
+    private fun alertUser(state: TimerState?) {
         viewModelScope.launch {
             when (state) {
-                TimerState.PREPARE -> beepHelper.startPrepareAlert()
-                TimerState.TRAIN -> beepHelper.startTrainAlert()
-                TimerState.REST -> beepHelper.startRestAlert()
-                TimerState.FINISHED -> beepHelper.finishedAlert()
-                else -> beepHelper.timerAlmostFinishingAlert()
+                TimerState.PREPARE -> alertUserHelper.startPrepareAlert()
+                TimerState.TRAIN -> alertUserHelper.startTrainAlert()
+                TimerState.REST -> alertUserHelper.startRestAlert()
+                TimerState.FINISHED -> alertUserHelper.finishedAlert()
+                else -> alertUserHelper.timerAlmostFinishingAlert()
             }
         }
     }
@@ -111,6 +167,13 @@ class TimerViewModel(
         val timerState : TimerState,
     )
 
+    sealed class Event {
+        object Prepare: Event()
+        object Train: Event()
+        object Rest: Event()
+        object Finished: Event()
+    }
+
     @Suppress("UNCHECKED_CAST")
     class Factory(
         val timerSettings: TimerSettings,
@@ -120,5 +183,9 @@ class TimerViewModel(
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return TimerViewModel(timerSettings, countDownTimerHelper, beepHelper) as T
         }
+    }
+
+    companion object {
+        private const val TAG = "TimerViewModel"
     }
 }
