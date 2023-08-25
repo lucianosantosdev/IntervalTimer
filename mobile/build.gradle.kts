c61fccdfdcf1050e0198356dbaed40528ab27c86
Luciano Santos <contato@lucianosantos.dev>
2023-08-25 09:49:18 -0300

    Fix app signing

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -14,9 +14,6 @@ val keystoreProperties = Properties().apply {
         keystoreFile.inputStream().use { fis ->
             load(fis)
         }
-        println("Keystore file found")
-    } else {
-        println("Keystore file not found")
     }
 }
 
@@ -46,7 +43,6 @@ android {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             if (!keystoreProperties.isEmpty) {
-                println("Apply signing config release")
                 signingConfig = signingConfigs.getByName("release")
             }
         }
