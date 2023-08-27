c000b4d77a63b0b06c600a8a073232b250a1f6c3
Luciano Santos <contato@lucianosantos.dev>
2023-08-27 13:55:03 -0300

    Change tags pattern

--- scripts/set_app_version.py
+++ scripts/set_app_version.py
@@ -14,16 +14,8 @@ def get_git_root():
 
 def get_version_from_git():
     git_repo = git.Repo(".", search_parent_directories=True)
-
-    try:
-        git_ref = git_repo.head.ref
-        if "release/" in git_ref.name:
-            return f'{git_ref.name.split("/")[1]}-RC'
-    except:
-        pass
-
-    last_tag = git_repo.git.describe("--tags")
-    return re.search(r'[0-9]+.[0-9]+.[0-9]+(-RC[0-9]*)?', last_tag)[0]
+    tags = sorted(git_repo.tags, key=lambda t: t.commit.committed_datetime)
+    return tags[-1]
         
 def set_app_version(version_name, version_code, target):
     gradle_file = f'{get_git_root()}/{target}/build.gradle.kts'
@@ -38,17 +30,10 @@ def set_app_version(version_name, version_code, target):
                 print(line, end ='')
                 
 def get_version_code(version_name, target):
-    version_match = re.findall(r'[0-9]+', version_name)
-    
-    major = int(version_match[0])
-    minor = int(version_match[1])
-    patch = int(version_match[2])
-    
-    version_number = r'{}{}{}'
     run_number = int(environ.get('GITHUB_RUN_NUMBER', 0))
     target_version = '10' if target == 'mobile' else '30'
     
-    return int(f'{major}{minor:02d}{patch:02d}{run_number:02d}{target_version}')
+    return int(f'{version_name}{run_number:02d}{target_version}')
        
 def main() -> int:
     target = sys.argv[1]
